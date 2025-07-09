d398c9488220d21f35bb93b0ea3e556d7f252936
goyalrajank <goyalrajank@gmail.com>
2025-07-09 13:53:22 +0530

    Update cuttingMaster.model.js

--- cuttingMaster.model.js
+++ cuttingMaster.model.js
@@ -1,1 +1,9 @@
-// Cutting master model
+const mongoose = require('mongoose');
+
+const cuttingMasterSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  lotRangeStart: { type: Number, required: true },
+  lotRangeEnd: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('CuttingMaster', cuttingMasterSchema);
